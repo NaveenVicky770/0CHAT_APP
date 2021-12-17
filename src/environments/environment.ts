421af632a51d86cbcfb01383da861578bcb1d737
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2021-12-17 16:45:47 +0530

    Initial Chat App commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDYi526SX9kRc9NGr_xRUXjSinr0NAQ1Lo",
+    authDomain: "chatappfire-b00a1.firebaseapp.com",
+    projectId: "chatappfire-b00a1",
+    storageBucket: "chatappfire-b00a1.appspot.com",
+    messagingSenderId: "802322073293",
+    appId: "1:802322073293:web:8414e6832e563b0acb8b84",
+    measurementId: "G-KWCPQYL8WZ",
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
